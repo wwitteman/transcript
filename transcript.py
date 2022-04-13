670bd658f72f9b13482bd065a66f5f5a848e8e1a
nuzwub <nuzwub@protonmail.com>
2022-04-12 23:02:40 -0400

    Getting started, finding out what isn't installed

---
+++ transcript.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+"""Use speech to text to record live transription and person identification."""
+
+import speech_recognition
+
+recog = speech_recognition.Recognizer()
+
+
+
+
+
+
+
